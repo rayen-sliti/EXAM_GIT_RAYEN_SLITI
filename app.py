25108b17fa858769aeb6623bf41e29f1034895dc
Rayen Sliti <slitirayen50@gmail.com>
2025-03-11 10:50:57 +0100

    Résolution du conflit après fusion de feature_x

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from datetime import datetime
 print("Hello, Git!")
-<<<<<<< HEAD
 print("Bienvenue dans ce projet Git!")
-=======
 print("Date et heure actuelles :", datetime.now())
->>>>>>> feature_x
+feature_x
